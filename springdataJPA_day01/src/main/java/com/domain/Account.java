45c7d796f3c2994e16072665f621c7be8a40b471
cuiye <598912698@qq.com>
2021-03-11 22:11:11 +0800

    111

---
+++ springdataJPA_day01/src/main/java/com/domain/Account.java
@@ -0,0 +1,49 @@
+package com.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+@Table(name = "account")
+public class Account implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "balance")
+    private Double balance;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
